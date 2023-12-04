2dd363f21f7c084a4f4ae4a1c3294734f00a878b
Pranavanesh-V <pranavanesh.vimalraj@gmail.com>
2023-12-04 17:25:28 +0530

    done

--- app/src/main/java/com/example/chatapplication/Security_page.java
+++ app/src/main/java/com/example/chatapplication/Security_page.java
@@ -2,13 +2,33 @@ package com.example.chatapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class Security_page extends AppCompatActivity {
 
+    ImageButton back;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_security_page);
+        back=findViewById(R.id.back);
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        Intent intent=new Intent(Security_page.this, Home_page.class);
+        startActivity(intent);
+        finish();
     }
 }
